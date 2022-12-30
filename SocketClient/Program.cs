278ce82e50dd6b2bebc254306ee5d1d00fff80f5
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2022-12-30 05:29:59 -0800

    Fix : fix bug in socket client to continue after entring the message

--- SocketClient/Program.cs
+++ SocketClient/Program.cs
@@ -1,24 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Socket Client is running ...");
+
 IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
 IPAddress ipAddress = ipHostInfo.AddressList[1];
-
 IPEndPoint ipEndPoint = new(ipAddress, 11_000);
 
+Console.WriteLine($"Server ip is {ipEndPoint}");
+Console.ResetColor();
+
 using Socket client = new(
-    ipEndPoint.AddressFamily, 
-    SocketType.Stream, 
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
     ProtocolType.Tcp);
 
 await client.ConnectAsync(ipEndPoint);
 
 while (true)
 {
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Enter your message : ");
+    Console.ResetColor();
     string message = Console.ReadLine();
+    
     
     while (true)
     {
@@ -26,26 +33,27 @@ while (true)
         message += "<|EOM|>";
         var messageBytes = Encoding.UTF8.GetBytes(message);
         _ = await client.SendAsync(messageBytes, SocketFlags.None);
-        Console.WriteLine($"Socket client sent message: \"{message}\"");
+        Console.WriteLine($"Socket client sent message: \"{message.Replace("<|EOM|>","")}\"");
 
         // Receive ack.
-        var buffer = new byte[1_024];
-        var received = await client.ReceiveAsync(buffer, SocketFlags.None);
-        var response = Encoding.UTF8.GetString(buffer, 0, received);
+        var ackBuffer = new byte[1_024];
+        var received = await client.ReceiveAsync(ackBuffer, SocketFlags.None);
+        var response = Encoding.UTF8.GetString(ackBuffer, 0, received);
         
-        if(response.Contains("acknowledgment"))
+        if(response.Contains("<|ACK|>"))
             break;
     
         // Sample output:
         //     Socket client sent message: "Hi friends 👋!<|EOM|>"
         //     Socket client received acknowledgment: "<|ACK|>"
     }
-    
+
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Press F1 to exit or press any key to continue ...");
     if(Console.ReadKey().Key == ConsoleKey.F1)
         break;
+    
+    Console.ResetColor();
 }
-
-
-
 
 client.Shutdown(SocketShutdown.Both);
--- SocketServer/Program.cs
+++ SocketServer/Program.cs
@@ -2,9 +2,15 @@
 using System.Net.Sockets;
 using System.Text;
 
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Socket Server is running ...");
+
 IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
 IPAddress ipAddress = ipHostInfo.AddressList[1];
 IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+
+Console.WriteLine($"Socket Server is listing on {ipEndPoint}");
+Console.ResetColor();
 
 using Socket listener = new(
     ipEndPoint.AddressFamily,
@@ -18,9 +24,9 @@ var handler = await listener.AcceptAsync();
 while (true)
 {
     // Receive message.
-    var buffer = new byte[1_024];
-    var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
-    var response = Encoding.UTF8.GetString(buffer, 0, received);
+    var ackBuffer = new byte[1_024];
+    var received = await handler.ReceiveAsync(ackBuffer, SocketFlags.None);
+    var response = Encoding.UTF8.GetString(ackBuffer, 0, received);
     
     var eom = "<|EOM|>";
     if (response.IndexOf(eom) > -1 /* is end of message */)
@@ -29,13 +35,13 @@ while (true)
             $"Socket server received message: \"{response.Replace(eom, "")}\"");
 
         var ackMessage = "<|ACK|>";
-        var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
-        await handler.SendAsync(echoBytes, 0);
+        var ackMessageBytes = Encoding.UTF8.GetBytes(ackMessage);
+        await handler.SendAsync(ackMessageBytes, 0);
         Console.WriteLine(
             $"Socket server sent acknowledgment: \"{ackMessage}\"");
-
-        if(Console.ReadKey().Key == ConsoleKey.F1)
-        break;
+        
+        Console.WriteLine();
+        
     }
     // Sample output:
     //    Socket server received message: "Hi friends 👋!"
